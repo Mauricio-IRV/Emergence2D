13732b746933cbb1eb9ef121c3a8c24f398c64de
Reece <reece.mirv@gmail.com>
2025-05-12 02:17:00 -0500

    Added level_3

--- emergence2d/scenes/next_level.gd
+++ emergence2d/scenes/next_level.gd
@@ -4,3 +4,4 @@ func _on_doorway_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		var next_scene_path = "res://scenes/level_" + str(int(get_tree().current_scene.name) + 1) + ".tscn"
 		get_tree().change_scene_to_file(next_scene_path)
+		print(next_scene_path)
