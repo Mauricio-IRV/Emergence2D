8de092b122ff284bc982819cc3476c524160b5cf
Reece <reece.mirv@gmail.com>
2025-05-12 18:07:38 -0500

    tweaks

--- emergence2d/tests/unit/test_death_checker.gd
+++ emergence2d/tests/unit/test_death_checker.gd
@@ -1,4 +1,11 @@
 extends "res://addons/gut/test.gd"
 
+const player_gd = preload("res://scripts/player.gd")
+var player: Player
+
+func before_each():
+	player = Player.new()
+
 func test_kill_player():
-	pass_test('nothing tested, passing')
+	player.die()
+	assert_true(player.health <= 0, "Player should be dead after calling die()")
