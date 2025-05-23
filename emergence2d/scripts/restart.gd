6d935c77cc5b26f1c2c4fb9fabbe45d6e558862d
Reece <reece.mirv@gmail.com>
2025-05-22 22:14:49 -0500

    Added collisions to rest of level 3 layout

--- emergence2d/scripts/restart.gd
+++ emergence2d/scripts/restart.gd
@@ -1,11 +1,11 @@
 extends Button
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+	
 func _pressed():
-	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+	get_tree().change_scene_to_file("res://scenes/level_3.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
