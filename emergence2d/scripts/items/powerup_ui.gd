0e371ab1aba86003c5f9acabe0e8cee4d7f1326d
Paul <paul@Pauls-MacBook-Pro-9.local>
2025-06-08 14:16:34 -0500

    changed controller

--- emergence2d/scripts/items/powerup_ui.gd
+++ emergence2d/scripts/items/powerup_ui.gd
@@ -3,7 +3,7 @@ extends Control
 @onready var lebel = $Label
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	EventController.connect("powerup_collected", on_event_powerup_collected)
+	GameController.connect("powerup_collected", on_event_powerup_collected)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
