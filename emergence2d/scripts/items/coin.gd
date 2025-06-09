9ae89500e514af5717457b0f537fd5826149f2e0
Reece <reece.mirv@gmail.com>
2025-06-08 23:33:25 -0500

    GameController update

--- emergence2d/scripts/items/coin.gd
+++ emergence2d/scripts/items/coin.gd
@@ -5,7 +5,6 @@ extends Node2D
 
 @onready var hot = get_node("../../CanvasLayer/hotbar")
 
-
 var player = null
 func _on_area_2d_body_entered (body):
 	if body.name == "Player":
--- emergence2d/scripts/items/coin_ui.gd
+++ emergence2d/scripts/items/coin_ui.gd
@@ -3,8 +3,7 @@ extends Control
 @onready var lebel = $Label
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	EventController.connect("coin_collected", on_event_coin_collected)
-
+	GameController.connect("coin_collected", on_event_coin_collected)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func on_event_coin_collected (value: int):
